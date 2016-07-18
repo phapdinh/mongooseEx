4884057a092ee9a0cbd76e210bd23810690c7c31
phapdinh <centralparkloop@yahoo.com>
2016-07-17 21:24:19 -0400

    added endpoints for post, put, and delete

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ var db = 'mongodb://localhost/example';
 
 mongoose.connect(db);
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
 app.get('/',function(req,res) {
 	res.send('happy to be here');
 })
@@ -38,6 +43,64 @@ app.get('/books/:id', function(req,res) {
 			res.json(book);
 		}
 	})
+})
+
+app.post('/book',function(req, res) {
+	var newBook = new Book();
+	
+	newBook.title = req.body.title;
+	newBook.author = req.body.author;
+	newBook.category = req.body.category;
+	
+	newBook.save(function(err,book) {
+		if(err) {
+			res.send(err.message);
+		}
+		else {
+			console.log(book);
+			res.send(book);
+		}
+	});
+})
+
+app.post('/book2', function(req, res) {
+	Book.create(req.body, function(err, book) {
+		if(err) {
+			res.send(err.message);
+		} else {
+			console.log(book);
+			res.send(book);
+		}
+	});
+})
+
+app.put('book/:id',function(req, res) {
+	Book.findOneAndUpdate({
+			_id:req.params.id
+		},
+		{ $set: {title: req.body.title}},
+		{upsert: true},
+		function(err, newBook) {
+			if(err) {
+				console.log(err.message);
+			} else {
+				res.status(204);
+			}
+		});
+})
+
+app.delete('/book/:id', function() {
+	Book.findOneAndRemove({
+		_id:req.params.id
+	}, function(err, book) {
+		if(err) {
+			res.send('error');
+		}
+		else {
+			console.log(book);
+			res.status(204);
+		}
+	});
 })
 
 var port = 8090;
