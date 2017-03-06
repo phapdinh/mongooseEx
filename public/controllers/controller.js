6029402f2b442724240e03aac68bf3a30dd302ae
Phap Dinh <centralparkloop@yahoo.com>
2017-03-06 11:11:04 -0500

    Update controller.js

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,7 +2,7 @@ var myApp = angular.module('myApp',[]);
 
 myApp.controller('AppCtrl',['$scope', '$http',
 	function($scope,$http) {
-		$scope.remove = function(id) {
+		$scope.remove = function() {
 			$http.delete('/book/' + $scope.id).success(function(response) {
 				var url = location.origin + '/books';
 				window.location.assign(url);
